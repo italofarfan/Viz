1db7e3e0e291080f9b65c9c148ca7e0e27c742e1
Italo Farfán <it.farfan@gmail.com>
2015-05-28 08:18:08 +0200

    compatibilidad Opera

--- coming-soon/js/script.js
+++ coming-soon/js/script.js
@@ -3,3 +3,19 @@ $(function () {
     date_end2 = new Date(2015, 6 - 1, 5);
     $('#defaultCountdown2').countdown({until: date_end2});
 });
+
+
+
+// Compatibilidad de navegadores
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement("style")
+ 
+    msViewportStyle.appendChild(
+        document.createTextNode(
+            "@-ms-viewport{width:auto!important}"
+        )
+    )
+ 
+    document.getElementsByTagName("head")[0].appendChild(msViewportStyle)
+}
+
