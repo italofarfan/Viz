09bd44706cc29de8449a129fe4b971d5f44f384c
Italo Farfán <it.farfan@gmail.com>
2015-05-28 20:46:15 +0200

    webkit o moz

--- coming-soon/js/script.js
+++ coming-soon/js/script.js
@@ -1,6 +1,6 @@
 $(function () {
     var austDay = new Date();
-    date_end2 = new Date(2015, 6 - 1, 5);
+    date_end2 = new Date(2015, 15 - 1, 5);
     $('#defaultCountdown2').countdown({until: date_end2});
 });
 
